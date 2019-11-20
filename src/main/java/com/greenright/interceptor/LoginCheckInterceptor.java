a2afc36b1eb8fcf79c41a71250913581a7546f04
kimjisoo <classy960516@gmail.com>
2019-11-20 16:40:57 +0900

    게시판 진짜 끝

---
+++ src/main/java/com/greenright/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,16 @@
+package com.greenright.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoginCheckInterceptor implements HandlerInterceptor {
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+      throws Exception {
+    if(request.getSession().getAttribute("loginUser") == null) {
+      response.sendRedirect("/greenright/auth/form");
+    }
+    return true;
+  }
+}
